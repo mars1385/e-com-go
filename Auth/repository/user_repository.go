5dd684ddd7b0bba06825fe4d19b586670e57e6cd
mars1385 <faceless1385@gmail.com>
2024-06-13 23:53:52 +0330

    fix repository

--- Auth/repository/user_repository.go
+++ Auth/repository/user_repository.go
@@ -1,1 +1,17 @@
 package repository
+
+import (
+	"context"
+
+	"github.com/mars1385/e-com-go/auth/ent"
+	"github.com/mars1385/e-com-go/auth/ent/user"
+)
+
+type User struct {
+	*ent.UserClient
+}
+
+func (u *User) GetById(id int, ctx context.Context) (*ent.User, error) {
+
+	return u.Query().Where(user.ID(id)).Only(ctx)
+}
